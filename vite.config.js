27bbc39f3fd2143658f19de0fd12b5cb5278baf5
김형규 <khg6160@hdene.co.kr>
2024-06-20 13:46:22 +0900

    [기본 설정] vite 설정 - env 수정

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-const PREFIX_ENV_VALUE = ['MARVEL', 'VITE'];
+// const PREFIX_ENV_VALUE = ['MARVEL', 'VITE'];
 
 export default defineConfig({
-  envDir: './env',
-  envPrefix: PREFIX_ENV_VALUE,
+  // env는 배포할 때 사용
+  // envDir: './env',
+  // envPrefix: PREFIX_ENV_VALUE,
   plugins: [react()],
   server: {
     port: 3000,
