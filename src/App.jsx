c364858f369a6f6f108f12d95defc3443255b5bc
김형규 <khg6160@hdene.co.kr>
2024-07-03 10:48:43 +0900

    #9 기타 작업 => footer css 생성 및 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import {Route, Routes} from 'react-router-dom';
 import './styles/base/font.scss';
 import './styles/base/common.scss';
 import './styles/pages/dashboard.scss';
+import './styles/pages/sub/hero.scss';
 import './styles/components/header.scss';
+import './styles/components/footer.scss';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import DashBoard from "./pages/DashBoard.jsx";
+import Hero from "./componets/sub/Hero.jsx";
 import mock from './mock/mockApiService.js';
 
 function App() {
@@ -13,6 +16,7 @@ function App() {
     <>
       <Routes>
           <Route path="*" element={<DashBoard/>}/>
+          <Route path="/hero" element={<Hero/>}/>
       </Routes>
     </>
   )
